638a385c5e76de2920205bbabab090e331cb5ea0
onnanoco <moe@onnano.co>
2023-05-23 19:49:04 +0900

    update

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# MOEスマートコントラクト(ERC721)
+# MOE Token(ERC721)
 
-- Name: MOE Character NFT
+- Name: MOE NFT
 - Symbol: MOENFT
 - TYPE : ERC721
 - Contract address: 
-- Hardhatを使用します。
+- Hardhat
 - SOLC version: 0.8.19
 
-## Dependencies
+## Env.
 
 ```bash
 npm install
@@ -18,12 +18,12 @@ vi .env
 ## Deploy
 
 ```bssh
-npx hardhat run --network {ethereum||goerli} "scripts/deploy.js"
+npx hardhat run --network {ethereum||sepolia} "scripts/deploy.js"
 ```
 
 ## Verify
 
 ```bash
-npx hardhat verify --constructor-args arguments.js --contract "contracts/MoeErc20.sol:MoeErc20" --network {ethereum||goerli} {CONTRACT_ADDRESS}
+npx hardhat verify --constructor-args arguments.js --contract "contracts/MoeErc20.sol:MoeErc20" --network {ethereum||sepolia} {CONTRACT_ADDRESS}
 ```
 
--- contracts/MoeErc721.sol
+++ contracts/MoeErc721.sol
@@ -40,6 +40,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+struct NftInfo {
+    bytes4 jsonId;
+    string uri;
+}
+
 contract MoeErc721 is ERC721 {
 
     using Counters for Counters.Counter;
@@ -48,22 +53,38 @@ contract MoeErc721 is ERC721 {
     uint256 public constant fee = 10**18;
     address public immutable moeErc20Address;
 
+    mapping (uint256 => NftInfo[]) public nftInfo;
+    
+
     constructor(string memory _name, string memory _symbol, address _moeErc20Address) ERC721(_name, _symbol) {
         moeErc20Address = _moeErc20Address;
     }
 
     // Modifier
-    modifier onlyOwner(uint256 tokenId) {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "MOE: caller is not token owner or approved");
+    modifier onlyOwner(uint256 _tokenId) {
+        require(_isApprovedOrOwner(msg.sender, _tokenId), "MOE: caller is not token owner or approved");
         _;
     }
 
-    function mint(address to) public virtual {
+    function mint(address _to, bytes4 _jsonId, string memory _uri) public {
         ERC20 moeErc20 = ERC20(moeErc20Address);
         require(moeErc20.balanceOf(msg.sender) >= fee, "MOE: insufficient MOE balance");
         moeErc20.transfer(address(0), fee);
-        _mint(to, _tokenIdTracker.current());
+        _mint(_to, _tokenIdTracker.current());
+        nftInfo[_tokenIdTracker.current()] = NftInfo(_jsonId, _uri);
         _tokenIdTracker.increment();
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        _requireMinted(_tokenId);
+
+        return nftInfo[_tokenId].uri;
+    }
+
+    function jsonId(uint256 _tokenId) public view returns (bytes4 memory) {
+        _requireMinted(_tokenId);
+
+        return nftInfo[_tokenId].jsonId;
     }
 
 
